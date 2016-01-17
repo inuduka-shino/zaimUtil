c39ad8d5762d31c9aa0c5a0d53a6058d6bf1a961
nknshm <hihinuma@gmail.com>
2016-01-17 15:03:00 +0900

    ofxtest sample

--- ofxtest.js
+++ ofxtest.js
@@ -3,17 +3,104 @@
 (function () {
     'use strict';
     const // config = require('./config'),
-        ofx = require('node-ofx'),
         fs = require('fs'),
-        ofxInfo = require('./ofxInfo/info'),
+        sampleData = require('./ofxInfo/sample');
 
-        header = ofxInfo.header,
-        body = ofxInfo.body;
+    let ofxData;
+
+
+    function genOfxData(info) {
+        const ofx = require('node-ofx'),
+            dateString = require('./lib/dateString'),
+            ofxInfo = require('./ofxInfo/info'),
+            header = ofxInfo.header,
+            body = ofxInfo.body,
+            transList = [],
+            nowStr = dateString.makeDayString(new Date(), 'YYYYMMDDhhmmss');
+
+        let transIdBase,
+            transListCount = 0,
+            firstDate,
+            lastDate,
+            lastAmount;
+
+
+        transIdBase = 'ZAIM' + nowStr;
+        body.SIGNONMSGSRSV1.SONRS.DTSERVER =  + '[+9:JST]';
+        Object.assign(body.SIGNONMSGSRSV1.SONRS.FI, {
+            ORG: info.fiOrg,
+            FID: info.fiFid
+        });
+        Object.assign(body.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKACCTFROM, {
+            BANKID: info.bankID,
+            BRANCHID: info.bankCID,
+            ACCTID: info.acctID,
+            ACCTTYPE: info.acctType
+        });
+
+        function addTrans(transInfo) {
+            /* transInfo = {
+                id,
+                type, 'DEBIT' or 'CREDIT'
+
+            }
+            */
+            const tranDate = transInfo.DTPOSTED;
+            let transId;
+
+            if (transInfo.id === undefined) {
+                transId = transIdBase + ('000' + transListCount).slice(-4);
+            } else {
+                transId = transIdBase + transInfo.id;
+            }
+            transList.push({
+                TRNTYPE: transInfo.type,
+                DTPOSTED: tranDate,
+                TRNAMT: transInfo.amount,
+                FITID: transId,
+                NAME: transInfo.name,
+                MEMO: transInfo.memo
+            });
+            transListCount += 1;
+            if (tranDate < firstDate || firstDate === undefined ) {
+                firstDate = tranDate;
+            }
+            if (tranDate > lastDate || lastDate === undefined ) {
+                lastDate = tranDate;
+            }
+        }
+        function serialize() {
+            lastAmount = 99999;
+
+            body.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.DTSTART = firstDate;
+            body.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.DTEND = lastDate;
+
+            Array.prototype.push.apply(
+                body.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN,
+                transList
+            );
+
+            body.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.BALAMT = String(lastAmount);
+            body.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.DTASOF = lastDate;
+
+            return ofx.serialize(header, body);
+        }
+
+        return {
+            addTrans: addTrans,
+            serialize: serialize
+        };
+    }
+
+    ofxData = genOfxData(sampleData);
+    sampleData.transactions.forEach((data) => {
+        ofxData.addTrans(data);
+    });
 
     console.log('writeing...');
     fs.writeFile(
-        'work/sampleOutput.ofx',
-        ofx.serialize(header, body),
+        'ofxInfo/sampleOutput.ofx',
+        ofxData.serialize(),
         {
             encoding: 'utf8'
         },
