f92ded4c85a101ed7faeba546048fa353cda4fb6
inuduka-shino <hihinuma@gmail.com>
2016-04-12 21:05:41 +0900

    getCategDict

---
+++ getCatD.js
@@ -0,0 +1,51 @@
+/*eslint no-console: 0 */
+
+(() => {
+    'use strict';
+    const
+        co = require('co'),
+        genAccessableZaim = require('./lib/genAccessableZaim'),
+        memoUtil = require('./lib/memo');
+
+    // main
+    co(function *() {
+        let zaim;
+        const config = require('./config');
+
+        console.log('start');
+
+        {
+            const memo = yield memoUtil('./memo.json').load();
+            zaim = yield genAccessableZaim({
+                consumerKey: config.consumerKey,
+                consumerSecret: config.consumerSecret,
+                accessToken: memo.info.accessToken,
+                accessTokenSecret: memo.info.accessTokenSecret
+            }, (newAccessToken) => {
+                memo.info.accessToken = newAccessToken.accessToken;
+                memo.info.accessTokenSecret = newAccessToken.accessTokenSecret;
+                return memo.save();
+            });
+        }
+
+        co(function* () {
+            // getCategory & getGenre
+            console.log('*getCategoryDict*');
+            let dict;
+            dict = yield zaim.getCategoryDict();
+            console.log('- 103');
+            console.dir(dict.get(103).category);
+            console.log('- 101');
+            console.dir(dict.get(101).category);
+        });
+
+    }).catch((err) => {
+        console.error('*** ERROR ***');
+        if (err.stack === undefined) {
+            console.error(err);
+        } else {
+            console.error(err.stack);
+        }
+    });
+
+})();
